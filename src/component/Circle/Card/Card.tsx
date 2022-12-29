a48fe6d9f079309a906d3bedf51da476585b43d3
Lord Mhyesch-Leegher Mike Mherllow's Mardy <82233085+LordMik3@users.noreply.github.com>
2022-12-29 11:26:49 -0500

    A little revamp
    Team and contributors done

--- src/component/Circle/Card/Card.tsx
+++ src/component/Circle/Card/Card.tsx
@@ -1,64 +1,90 @@
-import React from "react";
 import {
-  Heading,
-  Flex,
-  VStack,
-  Box,
-  WrapItem,
-  Wrap,
-  Center,
-  Image,
   Badge,
+  Center,
+  Flex,
+  Heading,
   Icon,
+  Image,
   useColorModeValue,
+  VStack,
+  Wrap,
+  WrapItem,
 } from "@chakra-ui/react";
-import { FaFacebook, FaTwitter, FaLinkedin } from "react-icons/fa";
+import Link from "next/link";
+import { FaFacebook, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 export default function Card() {
   return (
     <VStack spacing={0} w="100%" borderWidth="1px">
-      <Center w="100%" bg={useColorModeValue("gray.50", "gray.900")} p={10}>
-        <Flex flexDir="row" gap={5} h="100%">
+      {
+        //* Name and main role
+      }
+      <Center w="100%" bg={useColorModeValue("gray.50", "gray.900")} p={4}>
+        <Flex flexDir="column" alignItems="center">
           <Image
             objectFit="cover"
             fit="cover"
             align="center"
-            w="109px"
-            h="109px"
+            w="7rem"
+            h="7rem"
             src="https://images.unsplash.com/photo-1602233158242-3ba0ac4d2167?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=736&q=80"
             alt=""
           />
-          <Flex flexDir="column" justifyContent="space-between" flexGrow="1">
-            <Box>
-              <Heading size="lg">Jessica Nigri</Heading>
-              <Badge colorScheme="blue" borderRadius="lg">
-                Developer
-              </Badge>
-            </Box>
-            <Wrap>
-              <WrapItem>
-                <Icon as={FaFacebook} boxSize={5} color="facebook.500" />
-              </WrapItem>
-              <WrapItem>
-                <Icon as={FaTwitter} boxSize={5} color="twitter.500" />
-              </WrapItem>
-              <WrapItem>
-                <Icon as={FaLinkedin} boxSize={5} color="linkedin.500" />
-              </WrapItem>
-            </Wrap>
+          <Flex flexDir="column" alignItems="center" flexGrow="1">
+            <Heading size="lg">Jessica Nigri</Heading>
+            <Badge
+              colorScheme="blue"
+              borderRadius="lg"
+              px={3}
+              justifySelf="center"
+            >
+              Developer
+            </Badge>
           </Flex>
         </Flex>
       </Center>
-      <Center h="20%" background="blue" w="100%" p={3}>
+      {
+        //* Socials
+      }
+      <Center
+        h="20%"
+        background={useColorModeValue("gray.50", "gray.900")}
+        w="100%"
+        p={3}
+      >
+        <Wrap spacing={4}>
+          <WrapItem>
+            <Link href="#">
+              <Icon as={FaFacebook} boxSize={5} color="facebook.500" />
+            </Link>
+          </WrapItem>
+          <WrapItem>
+            <Icon as={FaTwitter} boxSize={5} color="twitter.500" />
+          </WrapItem>
+          <WrapItem>
+            <Icon as={FaLinkedin} boxSize={5} color="linkedin.500" />
+          </WrapItem>
+        </Wrap>
+      </Center>
+      {
+        //* Roles
+      }
+      <Center h="20%" background="blue.300" w="100%" p={3}>
         <Wrap>
           <WrapItem>
-            <Badge fontSize=".6em">Head Developper</Badge>
+            <Badge colorScheme="gray" borderRadius="lg" px={3}>
+              Head Developper
+            </Badge>
           </WrapItem>
           <WrapItem>
-            <Badge fontSize=".6em">Front End Developer</Badge>
+            <Badge colorScheme="gray" borderRadius="lg" px={3}>
+              Frontend
+            </Badge>
           </WrapItem>
           <WrapItem>
-            <Badge fontSize=".6em">Back End Developer</Badge>
+            <Badge colorScheme="gray" borderRadius="lg" px={3}>
+              Backend
+            </Badge>
           </WrapItem>
         </Wrap>
       </Center>
--- src/component/Circle/Circle.tsx
+++ src/component/Circle/Circle.tsx
@@ -1,26 +1,134 @@
-import React from "react";
 import {
-  Heading,
-  VStack,
+  Avatar,
   Box,
-  WrapItem,
-  Wrap,
+  Button,
+  Flex,
+  Grid,
+  GridItem,
+  Heading,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+  VStack,
 } from "@chakra-ui/react";
+import { FaDollarSign } from "react-icons/fa";
+import { useComponentSize } from "react-use-size";
 import Card from "./Card/Card";
+import ContributorCard from "./Card/ContributorCard";
 
 export default function Circle() {
+  const { ref, width } = useComponentSize();
+
   return (
-    <Box flexGrow="1" display="flex" alignItems="center" flexDir="column" pt={10}>
-      <VStack borderWidth="1px" w="4xl" spacing={4} borderRadius="lg" p={5}>
-        <Box w="100%">
+    <Box
+      flexGrow="1"
+      display="flex"
+      alignItems="center"
+      flexDir="column"
+      p={5}
+      gap={10}
+    >
+      {
+        // * Team presentation
+      }
+      <VStack
+        ref={ref}
+        borderWidth="1px"
+        w="max-content"
+        spacing={4}
+        borderRadius="lg"
+        p={5}
+      >
+        <Box w="100%" borderBottomWidth={1} pb={4}>
           <Heading size="md">Our Team</Heading>
         </Box>
-        <Wrap w="100%">
-          <WrapItem minW="50%" maxW="100%" w="50%">
+        <Grid w="100%" templateColumns={{ xl: "1fr 1fr", sm: "1fr" }} gap={5}>
+          <GridItem>
             <Card />
-          </WrapItem>
-        </Wrap>
+          </GridItem>
+          <GridItem>
+            <Card />
+          </GridItem>
+        </Grid>
       </VStack>
+      {
+        // * Fin team
+      }
+      {
+        // * Contributors
+      }
+      <VStack borderWidth={1} borderRadius="lg" p={5} spacing={4} w={width}>
+        <Box w="100%">
+          <Heading size="md">Contributors</Heading>
+        </Box>
+        <Tabs variant="unstyled" w="100%">
+          <TabList>
+            <Tab
+              textTransform="uppercase"
+              _selected={{ bg: "blue.300", color: "white" }}
+            >
+              Most recent
+            </Tab>
+            <Tab
+              textTransform="uppercase"
+              _selected={{ bg: "blue.300", color: "white" }}
+            >
+              Top contibutors
+            </Tab>
+          </TabList>
+
+          <TabPanels>
+            <TabPanel
+              p={5}
+              maxH="250px"
+              overflowY="scroll"
+              sx={{
+                "&::-webkit-scrollbar": {
+                  display: "none",
+                },
+                "-ms-overflow-style": "none",
+                scrollbarWidth: "none",
+              }}
+            >
+              <VStack w="100%" spacing={6}>
+                <ContributorCard />
+                <ContributorCard />
+                <ContributorCard />
+                <ContributorCard />
+                <ContributorCard />
+                <ContributorCard />
+                <ContributorCard />
+                <ContributorCard />
+                <ContributorCard />
+              </VStack>
+            </TabPanel>
+
+            <TabPanel
+              p={5}
+              maxH="250px"
+              overflowY="scroll"
+              sx={{
+                "&::-webkit-scrollbar": {
+                  display: "none",
+                },
+                "-ms-overflow-style": "none",
+                scrollbarWidth: "none",
+              }}
+            >
+              <VStack w="100%" spacing={6}>
+                <ContributorCard />
+                <ContributorCard />
+              </VStack>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </VStack>
+      {
+        // * Fin contributors
+      }
     </Box>
   );
 }
